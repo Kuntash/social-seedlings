ab82cdd27b621e959f4a1007b1e5581e5f07bee6
Kunga Tashi <kuntashtashi11@gmail.com>
2023-07-31 02:02:56 +0530

    blurhash implementatio

--- src/components/atoms/FeedImage/FeedImage.tsx
+++ src/components/atoms/FeedImage/FeedImage.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from '@main/components/atoms/FeedImage/FeedImage.module.css';
 import { FeedImageProps } from './types';
 import Image from 'next/legacy/image';
+import useBlurData from 'use-next-blurhash';
 
 export const FeedImage = (props: FeedImageProps) => {
-  const { urls, size, color, layout, width, height, alt } = props;
+  const { urls, size, color, layout, width, height, alt, blurHash } = props;
 
+  const [blurDataUrl] = useBlurData(blurHash, 5, 5);
   return (
     <div
       className={`${styles.feed__image__container} ${styles?.[size]}
@@ -16,6 +18,8 @@ export const FeedImage = (props: FeedImageProps) => {
         src={urls?.[size]}
         alt={alt}
         layout={layout}
+        placeholder="blur"
+        blurDataURL={blurDataUrl}
         {...(width && { width })}
         {...(height && { height })}
         objectFit="cover"
